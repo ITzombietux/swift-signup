fe2d2c0e8256ff91dfb8ee47ce107e4982f624b9
zombietux <>
2021-03-29 14:31:41 +0900

    Models 추가

---
+++ SignUpApp/SignUpApp/Models/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  SignUpApp
+//
+//  Created by zombietux on 2021/03/29.
+//
+
+import Foundation
+
+struct User {
+    private let id: String
+    private let password: String
+    private let information: Information
+}
