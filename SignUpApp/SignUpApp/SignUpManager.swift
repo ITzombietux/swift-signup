493fb55ccb683e3d5c4ba6bfe4b057ff19be7366
zombietux <>
2021-03-29 17:43:15 +0900

    storybard 레이아웃 수정

---
+++ SignUpApp/SignUpApp/SignUpManager.swift
@@ -0,0 +1,8 @@
+//
+//  SignUpManager.swift
+//  SignUpApp
+//
+//  Created by zombietux on 2021/03/29.
+//
+
+import Foundation
---
+++ SignUpApp/SignUpApp/SugnUpViewControllerDelegate.swift
@@ -0,0 +1,8 @@
+//
+//  SugnUpViewControllerDelegate.swift
+//  SignUpApp
+//
+//  Created by zombietux on 2021/03/29.
+//
+
+import Foundation
