1c98c693ec3e11d9920e242aa2cc630cbf9cce2e
zombietux <>
2021-03-30 11:18:17 +0900

    User 메서드 프로토콜 추가

--- SignUpApp/SignUpApp/Models/UserManageable.swift
+++ SignUpApp/SignUpApp/Models/UserManageable.swift
@@ -6,3 +6,10 @@
 //
 
 import Foundation
+
+protocol UserManageable {
+    func isValidId(id: String) -> Bool
+    func isValidPassword(pwd: String) -> Bool
+    func isValidEmail(email: String) -> Bool
+    func isValidPhoneNumber(phoneNumber: String) -> Bool
+}
